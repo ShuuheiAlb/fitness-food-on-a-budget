a4e0481345b0d00473027f4530679f3f7e04fd3a
ShuuheiAlb <eagleluph@gmail.com>
2024-06-05 16:26:59 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,73 @@
 #%%
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
 from matplotlib.patches import FancyBboxPatch
 from os.path import getmtime
 from datetime import datetime
 
-# Importing data
 # Supermarket name appearing on variables are aliased as "supa" (Woolworths) and "supb" (Coles)
-@st.cache_resource
-def load_data(path):
-    return pd.read_csv(path, header=0)
-
 @st.cache_data
-def merged_average(df1, df2):
-    df_concat = pd.concat([df1, df2])
+def load_data():
+    df_a = pd.read_csv("out/supa_out.csv", header=0)
+    df_b = pd.read_csv("out/supb_out.csv", header=0)
+    df_concat = pd.concat([df_a, df_b])
     return df_concat.groupby(df_concat.columns[:-1].tolist()).mean().reset_index()
 
-@st.cache_data
-def select_category(df, category):
-    return df[df["Category"] == category]
+# Plot based on macronutrient selection
+@st.cache_data()
+def select_plot(df, category):
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sub_df = df[df["Category"] == category]
+    sns.barplot(x='Amount', y='Food', hue='Food', legend=False,
+                data=sub_df.sort_values(by="Amount", ascending=False),
+                palette=sns.color_palette(palette="pastel", n_colors=len(sub_df)))
 
-supa = load_data("out/supa_out.csv")
-supb = load_data("out/supb_out.csv")
-df = merged_average(supa, supb)
+    # Styling
+    sns.set_style("white")
+    sns.despine(left=True, bottom=True)
 
-# CSS: manually put it since Streamlit Deployment makes "the other way" much more complicated
-# Otherwise for offline use, style.css can be used
+    # Ticks/labels
+    ax.set(xlabel="", ylabel="")
+    ax.tick_params(axis='x', bottom=False, labelbottom=False)
+    ax.tick_params(axis='y', left=False, pad=10, labelsize="x-large", labelcolor="#606060")
+    for i in ax.containers:
+        ax.bar_label(i, fmt="%.1f", padding=15, size="x-large", color="#606060")
+
+    # Make rounded bars
+    new_patches = []
+    for patch in reversed(ax.patches):
+        bb = patch.get_bbox()
+        color = patch.get_facecolor()
+        p_bbox = FancyBboxPatch((bb.xmin, bb.ymin), abs(bb.width), abs(bb.height),
+                                boxstyle="round, pad=0.01, rounding_size=0.2", mutation_aspect=0.7,
+                                ec="none", fc=color)
+        patch.remove()
+        new_patches.append(p_bbox)
+    for patch in new_patches:
+        ax.add_patch(patch)
+
+    return {"fig": fig, "ax": ax}
+
+# ===
+
+# Inline CSS it since Streamlit Deploy makes style.css much more complicated
 css_str = '[class^="st-emotion"]  { font-weight: 500; }'
 st.markdown( f'<style>{css_str}</style>' , unsafe_allow_html= True)
 
-# Title
 st.title("Fitness Food on a Budget")
 
-# Current variables
-curr_cat = st.selectbox(
-   "Macronutrient (in gram/$)",
-   ("protein", "carb", "fat", "fruit", "vegetable"),
-   index=0,
-   placeholder="Select"
-)
-curr_data = select_category(df, curr_cat)
+df = load_data()
+curr_cat = st.selectbox("Macronutrient (in gram/AUD)",
+                        ("protein", "carb", "fat", "fruit", "vegetable"),
+                        index=0, placeholder="Select")
+plt_obj = select_plot(df, curr_cat)
 
-# Main plot, styling, ticks/labels
-fig = plt.figure(figsize=(12, 6))
-ax = sns.barplot(x='Amount', y='Food', hue='Food', data=curr_data.sort_values(by="Amount", ascending=False),
-               palette=sns.color_palette(palette="pastel", n_colors=len(curr_data)), legend=False)
+st.pyplot(plt_obj["fig"])
 
-sns.set_style("white")
-sns.despine(left=True, bottom=True)
-
-# Make rounded bars
-new_patches = []
-for patch in reversed(ax.patches):
-    bb = patch.get_bbox()
-    color = patch.get_facecolor()
-    p_bbox = FancyBboxPatch(
-        (bb.xmin, bb.ymin), abs(bb.width), abs(bb.height),
-        boxstyle="round, pad=-0.005, rounding_size=0.015", mutation_aspect=0.2,
-        ec="none", fc=color,
-    )
-    patch.remove()
-    new_patches.append(p_bbox)
-for patch in new_patches:
-    ax.add_patch(patch)
-
-ax.set(xlabel="", ylabel="")
-ax.tick_params(axis='x', bottom=False, labelbottom=False)
-ax.tick_params(axis='y', left=False, pad=10, labelsize="x-large", labelcolor="#606060")
-for i in ax.containers:
-    ax.bar_label(i, fmt="%.1f", padding=15, size="x-large", color="#606060")
-
-st.pyplot(fig)
-
-# Last update time: should do the supb soon too
-st.caption(f"*(as of {datetime.fromtimestamp(getmtime('out/supa_out.csv')).strftime('%d %b %Y')})*")
+# Should do the supb soon too
+last_update_time = datetime.fromtimestamp(getmtime("out/supa_out.csv"))
+st.caption(f"*(as of {last_update_time.strftime('%d %b %Y')})*")
 
 # %%
